8688b48dcbdceb76b1f66ac3f6c3342e6b95ff1f
Abhay Parashar <52857736+Abhayparashar31@users.noreply.github.com>
2022-06-26 19:45:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,24 @@ import imdb_scraper as imdb
 import hydralit_components as hc
 import time
 
+st.set_page_config(
+    page_title = "IMDB Movies Recommender System",
+    page_icon = "🎞️"
+)
+
+st.markdown(
+    """
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    """,
+    unsafe_allow_html=True,
+)
+
+
 ### Adding Title and Subtitle For Web App
 st.title('Movie Recommender System')
 st.text('A System That Recommend Movies For User.')
+st.image('data/home.gif')
 
 ### Unpickling Pickled Data
 df = pickle.load(open('df.pkl','rb'))
